4178c236cdc2561da958e5af16f41416d0824e11
Bagire <ocelot@list.ru>
2015-03-22 23:06:12 +0600

    HW13
    Домашняя работа 13

---
+++ generate.bat
@@ -0,0 +1,8 @@
+java -cp bin com.example.tests.GroupDataGenerator 2 groups.txt csv
+
+java -cp bin;C:\Distrib\xstream-1.4.8\lib\xstream-1.4.8.jar;C:\Distrib\xstream-1.4.8\lib\xstream\xpp3_min-1.1.4c.jar com.example.tests.GroupDataGenerator 4 groups.xml xml
+
+java -cp bin com.example.tests.GroupDataGenerator 3 contacts.txt csv
+
+java -cp bin;C:\Distrib\xstream-1.4.8\lib\xstream-1.4.8.jar;C:\Distrib\xstream-1.4.8\lib\xstream\xpp3_min-1.1.4c.jar com.example.tests.GroupDataGenerator 5 contacts.xml xml
+
