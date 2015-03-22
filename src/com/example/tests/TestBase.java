632e44767ba5293316eff88a64cd3f14fc34e706
Bagire <ocelot@list.ru>
2015-03-23 00:08:31 +0600

    HW14
    Домашняя работа 14

--- src/com/example/tests/TestBase.java
+++ src/com/example/tests/TestBase.java
@@ -1,8 +1,11 @@
 package com.example.tests;
 
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -12,13 +15,17 @@ import com.example.fw.ApplicationManager;
 
 import static com.example.tests.GroupDataGenerator.generateRandomGroups;
 import static com.example.tests.ContactDataGenerator.generateRandomContacts;
+
 public class TestBase {
 
 	protected static ApplicationManager app;
 	
 	@BeforeTest
 	public void setUp() throws Exception {
-		app = new ApplicationManager();
+		String configFile = System.getProperty("configFile", "application.properties");
+		Properties properties = new Properties();
+		properties.load(new FileReader(new File(configFile)));
+		app = new ApplicationManager(properties);
 	  }
 
 	@AfterTest
@@ -38,7 +45,6 @@ public class TestBase {
 	  }
 	  return list;
 	}
-
 
 	@DataProvider
 	public Iterator<Object[]> randomValidContactGenerator(){
